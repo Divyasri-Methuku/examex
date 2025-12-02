76119d857a54658a415aa8349dff7c7b15f42998
saisreekondu <saisreekondu3103@gmail.com>
2025-12-02 10:05:54 +0530

    success

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from Flask import flask,render_template,request,redirect,url_for
-app=Flask(__name__)
-@app.route('\',method=['GET','POST'])
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-	if request.form=='POST':
-		print("Successfull")
-		return "successfull registration"
-	return render_template('register.html')
-if __name__='__main__':
-	app.run(debug=True)
+    if request.method == 'POST':
+        print("Form submitted successfully!")
+        print("Form Data:", request.form)
+        return "Registration successful!"  
+    return render_template('register.html')
+
+if __name__ == '__main__':
+     app.run(debug=True)
